aa471e5d93183f06461b866ea87c69c235226ad5
Vlad Serdiuk <vladus629@gmail.com>
2022-04-04 20:51:55 +0300

    Practice-pt2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMoviesDB = {
@@ -8,12 +10,27 @@ const personalMoviesDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотернных фильмов?', ''),
-      b = prompt('На сколько вы его оцените?', ''),
-      c = prompt('Один из последних просмотернных фильмов?', ''),
-      d = prompt('На сколько вы его оцените?', '');
+for (let i=0; i < 2; i++) {
+    const a = prompt('Один из последних просмотернных фильмов?', ''),
+          b = prompt('На сколько вы его оцените?', '');
 
-personalMoviesDB.movies[a] = b;
-personalMoviesDB.movies[c] = d;
+    if (a != null && b !== null && a != '' && b != ''  && a.length < 50) {
+        personalMoviesDB.movies[a] = b; 
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }  
+}
+
+if (personalMoviesDB.count < 10) {
+    console.log();
+} else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMoviesDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMoviesDB);
